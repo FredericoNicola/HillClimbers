cadded70f71daffac1788dfb81e2d66b185f58f7
FredericoNicola <fredericonicola@gmail.com>
2021-12-26 15:36:23 +0000

    Generate pages from database info

--- app/src/main/java/com/example/hillclimbers/RoadDetails.kt
+++ app/src/main/java/com/example/hillclimbers/RoadDetails.kt
@@ -1,4 +1,11 @@
 package com.example.hillclimbers
 
-class RoadDetails {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RoadDetails : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.road_details)
+    }
 }
